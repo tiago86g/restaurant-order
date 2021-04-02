623c75d2f9ae3a27009f3a599d69b7a02035cc0b
tiago86g <tiago86g@gmail.com>
2021-04-02 16:56:38 +0200

    router structure

---
+++ src/pages/Meal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Meal = () => {
+  return (
+    <div>
+      <p>Meal</p>
+    </div>
+  );
+};
---
+++ src/pages/Receipt.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Receipt = () => {
+  return (
+    <div>
+      <p>Receipt</p>
+    </div>
+  );
+};
