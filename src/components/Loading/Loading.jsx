adb434821d506f4e60e47e3c94c5d41e0fc6e0eb
tiago86g <tiago86g@gmail.com>
2021-04-02 18:07:46 +0200

    fetch random meal

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div>
+      <p>Is loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Loading } from '../components';
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meal, setMeal] = useState('');
+
+  const fetchRandomMeal = async () => {
+    const res = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/random.php',
+    );
+    res
+      .json()
+      .then((res) => setMeal(res.meals[0]))
+      .catch((err) => console.error(err));
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRandomMeal();
+  }, []);
+
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <div>{isLoading ? <Loading /> : <p>{`Meal: ${meal.strMeal}`}</p>}</div>
   );
 };
