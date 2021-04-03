33b4b407f3c97a8c7f582cd315cacaa72ba4d890
tiago86g <tiago86g@gmail.com>
2021-04-03 18:58:37 +0200

    receipt details

--- src/pages/Receipt.jsx
+++ src/pages/Receipt.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export const Receipt = () => {
   return (
     <div>
-      <p>Receipt</p>
+      <h2>Receipt</h2>
+      <h3>Email:</h3>
+      <h3>When:</h3>
+      <h3>Number of guests:</h3>
     </div>
   );
 };
