62f3bdb71b2ac4205af70c85d595a6765bfcfa28
tiago86g <tiago86g@gmail.com>
2021-04-04 10:07:16 +0200

    removing unused footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Footer } from './Footer/Footer';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Loading } from './Loading/Loading';
-export {default as ImageSlider} from './ImageSlider/ImageSlider.jsx';
+export { default as ImageSlider } from './ImageSlider/ImageSlider.jsx';
